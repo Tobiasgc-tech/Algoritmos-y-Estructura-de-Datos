9d8fb45798eac5feeb4ea4883c1225eeb2960da1
Tobias Cogliano <80077949+Tobiasgc-tech@users.noreply.github.com>
2024-04-26 19:22:54 -0300

    Add files via upload

---
+++ taller3/src/main/java/aed/Fecha.java
@@ -0,0 +1,44 @@
+package aed;
+
+public class Fecha {
+    public Fecha(int dia, int mes) {
+        int _dia = dia;
+        int _mes = mes;
+    }
+
+    public Fecha(Fecha fecha) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public Integer dia() {
+        return _dia;
+    }
+
+    public Integer mes() {
+        return _mes;
+    }
+
+    public String toString() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    @Override
+    public boolean equals(Object otra) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void incrementarDia() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    private int diasEnMes(int mes) {
+        int dias[] = {
+                // ene, feb, mar, abr, may, jun
+                31, 28, 31, 30, 31, 30,
+                // jul, ago, sep, oct, nov, dic
+                31, 31, 30, 31, 30, 31
+        };
+        return dias[mes - 1];
+    }
+
+}
