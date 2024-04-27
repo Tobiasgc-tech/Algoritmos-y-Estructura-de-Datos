7ce1f810a3f632d59f4f2bd38922f0ef2a51e894
Tobiasgc <tobiasgabrielcogliano@gmail.com>
2024-04-26 22:17:25 -0300

    Agregue taller3

---
+++ Talleres/taller3/src/main/java/aed/Fecha.java
@@ -0,0 +1,67 @@
+package aed;
+
+public class Fecha {
+    private int _dia;
+    private int _mes;
+    public Fecha(int dia, int mes) {
+        this._dia = dia;
+        this._mes = mes;
+    }
+
+    public Fecha(Fecha fecha) {
+        this._dia = fecha.dia();
+        this._mes = fecha.mes();
+    }
+
+    public Integer dia() {
+        return _dia;
+    }
+
+    public Integer mes() {
+        return _mes;
+    }
+
+    public String toString() {
+        StringBuffer sbuffer = new StringBuffer();
+        sbuffer.append(dia().toString());
+        sbuffer.append("/");
+        sbuffer.append(mes().toString());
+        return sbuffer.toString();
+    }
+
+    @Override
+    public boolean equals(Object otra) {
+            boolean oen = (otra == null);
+            boolean cd = otra.getClass() != this.getClass();
+            if (oen || cd) {
+            return false;
+            }
+            Fecha otraFecha = (Fecha) otra;
+            return _dia == otraFecha._dia 
+                && _mes == otraFecha._mes;
+    }
+
+    public void incrementarDia() {
+         int diasEnEsteMes = diasEnMes(_mes);
+         if (_dia == diasEnEsteMes) {
+            _dia = 1;
+            _mes++;
+            if (_mes > 12){
+                _mes = 1;
+            }
+        } else{
+            _dia++;
+        }
+    }
+
+    private int diasEnMes(int mes) {
+        int dias[] = {
+                // ene, feb, mar, abr, may, jun
+                31, 28, 31, 30, 31, 30,
+                // jul, ago, sep, oct, nov, dic
+                31, 31, 30, 31, 30, 31
+        };
+        return dias[mes - 1];
+    }
+
+}
